b07f12e4f7e97e065f46d04fe0f4cef14b804a03
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2021-04-10 12:48:47 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
             <h1 className="text-5xl font-bold font-sans">Shad</h1>
             <p className="mt-2">discord bot developer</p>
             <div className="block mt-4">
-                <a href="https://discord.com/profile/298014369533657090" className="duration-75 p-2 hover:bg-opacity-25 font-lg mr-3 pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Discord</a>
+                <a href="https://discord.com/users/298014369533657090" className="duration-75 p-2 hover:bg-opacity-25 font-lg mr-3 pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Discord</a>
                 <a href="https://twitter.com/TypicallyShadow" className="duration-75 p-2 hover:bg-opacity-25 font-lg pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Twitter</a>
             </div>
         </div>
