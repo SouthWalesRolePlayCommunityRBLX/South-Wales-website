9a080047ce0d063a406029e61066713ac409f973
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2021-04-19 18:46:21 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,51 @@ export default function Home() {
     <>
         <Particles
             canvasClassName="particles"
-            height="1000px"
-            width="1000px"
             params={{
                 particles: {
                     move: {
-                        speed: 0.5
+                        speed: 3
+                    },
+                    size: {
+                        value: 20,
+                        random: true
+                    }
+                },
+                interactivity: {
+                    detectsOn: 'window',
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'repulse'
+                        },
+                        onclick: {
+                            enable: true,
+                            mode: 'bubble'
+                        }
+                    },
+                    modes: {
+                        bubble: {
+                            distance: 400,
+                            size: 20,
+                            duration: 2
+                        },
+                        repulse: {
+                            distance: 150
+                        }
                     }
                 }
             }} />
 
-        <div className="flex flex-col h-screen my-auto items-center bg-cover justify-center align-items-cente text-white">
-            <h1 className="text-5xl font-bold font-sans">Shad</h1>
-            <p className="mt-2">discord bot developer</p>
-            <div className="block mt-4">
-                <a href="https://discord.com/users/298014369533657090" className="duration-75 p-2 hover:bg-opacity-25 font-lg mr-3 pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Discord</a>
-                <a href="https://twitter.com/TypicallyShadow" className="duration-75 p-2 hover:bg-opacity-25 font-lg pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Twitter</a>
+        <div className="flex flex-col h-screen my-auto items-center bg-cover justify-center align-items-center text-white">
+            <div className="bg-black bg-opacity-20 p-20 rounded align-items-center items-center text-center">
+                <div className="block">
+                    <h1 className="text-5xl inline font-bold font-sans">Shad</h1>
+                    <span className="inline text-gray-300">#3200</span>
+                </div>
+                <div className="block mt-4">
+                    <a href="https://discord.com/users/298014369533657090" className="duration-75 p-2 hover:bg-opacity-25 font-lg mr-3 pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Portfolio</a>
+                    <a href="https://twitterr.com/TypicallyShad" className="duration-75 p-2 hover:bg-opacity-25 font-lg mr-3 pl-12 pr-12 rounded bg-white bg-opacity-10 border-white border-2">Twitter</a>
+                </div>
             </div>
         </div>
     </>
